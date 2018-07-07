85187005c6bdde23bd2cb85c6036b32bf0388b78
vaughnjg <vaughnjg@appstate.edu>
2018-07-07 13:12:52 -0400

    Update tutorial.cc

--- tutorial.cc
+++ tutorial.cc
@@ -1,4 +1,6 @@
 #include <sc2api/sc2_api.h>
+#include "sc2lib/sc2_lib.h"
+#include <iterator>
 
 #include <iostream>
 
@@ -6,63 +8,85 @@ using namespace sc2;
 
 class Bot : public Agent {
 public:
+
     virtual void OnGameStart() final {
-        std::cout << "Hello, World!" << std::endl;
+        std::cout << "Game Started." << std::endl;
+        
     }
 
     virtual void OnStep() final {
         TryBuildSupplyDepot();
         TryBuildVespeneGas();
         TryBuildBarracks();
+        TryBuildFactory();
+        TryBuildStarPort();
+        //TryExpand(ABILITY_ID::BUILD_COMMANDCENTER, UNIT_TYPEID::TERRAN_SCV);
+        TryBuildEngineerBay();
+        TryBuildArmory();
     }
 
     virtual void OnUnitIdle(const Unit* unit) final {
         switch (unit->unit_type.ToType()) {
         case UNIT_TYPEID::TERRAN_COMMANDCENTER: {
-            if (CountUnitType(UNIT_TYPEID::TERRAN_SCV) < 15) {
+            if (CountUnitType(UNIT_TYPEID::TERRAN_SCV) < 22) {
                 Actions()->UnitCommand(unit, ABILITY_ID::TRAIN_SCV);
             }    
             break;
         }
         case UNIT_TYPEID::TERRAN_SCV: {
             const Unit* mineral_target = FindNearestMineralPatch(unit->pos);
-            const Unit* vespene_target = FindNearestVespeneGas(unit->pos);
+            
             if (!mineral_target) {
                 break;
             }
-            if (!vespene_target) {
-                break;
-            }
+            
             Actions()->UnitCommand(unit, ABILITY_ID::SMART, mineral_target);
-            Actions()->UnitCommand(unit, ABILITY_ID::SMART, vespene_target);
+            
             break;
         }
         case UNIT_TYPEID::TERRAN_BARRACKS: {
+            
             Actions()->UnitCommand(unit, ABILITY_ID::TRAIN_MARINE);
             break;
         }
         case UNIT_TYPEID::TERRAN_MARINE: {
+            float rx = GetRandomScalar();
+            float ry = GetRandomScalar();
+
             const GameInfo& game_info = Observation()->GetGameInfo();
-            Actions()->UnitCommand(unit, ABILITY_ID::ATTACK_ATTACK, game_info.enemy_start_locations.front());
+            Actions()->UnitCommand(unit, ABILITY_ID::MOVE, Point2D(rx * 15.0f,ry * 15.0f));
+            if (CountUnitType(UNIT_TYPEID::TERRAN_MARINE) > 20) {
+                Actions()->UnitCommand(unit, ABILITY_ID::ATTACK_ATTACK, game_info.enemy_start_locations.front());
+            }
+            break;
+        }
+        case UNIT_TYPEID::TERRAN_STARPORT: {
+
+            Actions()->UnitCommand(unit, ABILITY_ID::TRAIN_MEDIVAC);
+            break;
+        }
+        case UNIT_TYPEID::TERRAN_FACTORY: {
+
+            //Actions()->UnitCommand(unit, ABILITY_ID::TRAIN_WIDOWMINE);
             break;
         }
         default: {
             break;
         }
         }
+
     }
 private:
+
     size_t CountUnitType(UNIT_TYPEID unit_type) {
         return Observation()->GetUnits(Unit::Alliance::Self, IsUnit(unit_type)).size();
     }
-
-    bool TryBuildStructure(ABILITY_ID ability_type_for_structure, UNIT_TYPEID unit_type = UNIT_TYPEID::TERRAN_SCV) {
+    bool TryBuildStructure(AbilityID ability_type_for_structure, UnitTypeID unit_type, Point2D location) {
         const ObservationInterface* observation = Observation();
 
-        // If a unit already is building a supply structure of this type, do nothing.
-        // Also get an scv to build the structure.
         const Unit* unit_to_build = nullptr;
         Units units = observation->GetUnits(Unit::Alliance::Self);
+
         for (const auto& unit : units) {
             for (const auto& order : unit->orders) {
                 if (order.ability_id == ability_type_for_structure) {
@@ -75,6 +99,45 @@ private:
             }
         }
 
+        
+
+        Actions()->UnitCommand(unit_to_build, ability_type_for_structure, location);
+        return true;
+
+
+    }
+    bool TryBuildStructure(ABILITY_ID ability_type_for_structure, UNIT_TYPEID unit_type = UNIT_TYPEID::TERRAN_SCV) {
+        const ObservationInterface* observation = Observation();
+
+        const Unit* unit_to_build = nullptr;
+        Units units = observation->GetUnits(Unit::Alliance::Self);
+
+        for (const auto& unit : units) {
+            for (const auto& order : unit->orders) {
+                if (order.ability_id == ability_type_for_structure) {
+                    return false;
+                }
+            }
+
+            if (unit->unit_type == unit_type) {
+                unit_to_build = unit;
+            }
+        }
+
+
+        if (ability_type_for_structure == ABILITY_ID::BUILD_REFINERY)
+        {
+            const Unit* vespene_target = FindNearestVespeneGas(unit_to_build->pos);
+            Actions()->UnitCommand(unit_to_build, ability_type_for_structure, vespene_target);
+            return true;
+        }
+
+      //  if (ability_type_for_structure == ABILITY_ID::BUILD_COMMANDCENTER){
+         //   Point3D close_exp = FindClosestExpansion(ABILITY_ID::BUILD_COMMANDCENTER);
+            
+         //   Actions()->UnitCommand(unit_to_build, ability_type_for_structure, Point2D(close_exp));
+           // return true;
+       // }
         float rx = GetRandomScalar();
         float ry = GetRandomScalar();
 
@@ -88,17 +151,15 @@ private:
     bool TryBuildSupplyDepot() {
         const ObservationInterface* observation = Observation();
 
-        // If we are less than 80 % capacity, do not build a supply depot
-        if (observation->GetFoodUsed() <= observation->GetFoodCap() * 0.8)
+        if (observation->GetFoodCap()  == 200)
             return false;
 
         // Try and build a depot. Find a random SCV and give it the order.
         return TryBuildStructure(ABILITY_ID::BUILD_SUPPLYDEPOT);
     }
     bool TryBuildVespeneGas() {
-        const ObservationInterface* observation = Observation();
 
-        if (CountUnitType(UNIT_TYPEID::NEUTRAL_VESPENEGEYSER) > 2) {
+        if (CountUnitType(UNIT_TYPEID::TERRAN_REFINERY) > 1) {
             return false;
         }
 
@@ -136,20 +197,73 @@ private:
         }
         return target;
     }
+    const ObservationInterface* observation = Observation();
+    std::vector<Point3D> expansions_ = search::CalculateExpansionLocations(Observation(), Query());
+    Point3D startLocation_ = Observation()->GetStartLocation();
+    bool TryExpand(AbilityID build_ability, UnitTypeID worker_type) {
+        
+        //Point3D staging_location_ = startLocation_;
+        const ObservationInterface* observation = Observation();
+        float min_distance = std::numeric_limits<float>::max();
+        
+        Point3D closest_expansion;
+        for (const auto& expansion : expansions_) {
+            float current_distance = Distance2D(startLocation_, expansion);
+            if (current_distance < .01f) {
+                continue;
+            }
+
+            if (current_distance < min_distance) {
+                if (Query()->Placement(build_ability, expansion)) {
+                    closest_expansion = expansion;
+                    min_distance = current_distance;
+                }
+            }
+        }
+        return TryBuildStructure(build_ability, worker_type, closest_expansion);
+    }
 
     bool TryBuildBarracks() {
-        const ObservationInterface* observation = Observation();
 
         if (CountUnitType(UNIT_TYPEID::TERRAN_SUPPLYDEPOT) < 1) {
             return false;
         }
 
-        if (CountUnitType(UNIT_TYPEID::TERRAN_BARRACKS) > 3) {
+        if (CountUnitType(UNIT_TYPEID::TERRAN_BARRACKS) > 2) {
             return false;
         }
 
         return TryBuildStructure(ABILITY_ID::BUILD_BARRACKS);
     }
+
+    bool TryBuildFactory() {
+        if (CountUnitType(UNIT_TYPEID::TERRAN_FACTORY) > 1) {
+            return false;
+        }
+        return TryBuildStructure(ABILITY_ID::BUILD_FACTORY);
+    }
+    bool TryBuildStarPort() {
+        if (CountUnitType(UNIT_TYPEID::TERRAN_STARPORT) > 1) {
+            return false;
+        }
+        return TryBuildStructure(ABILITY_ID::BUILD_STARPORT);
+    }
+    bool TryBuildEngineerBay() {
+        if (CountUnitType(UNIT_TYPEID::TERRAN_ENGINEERINGBAY) > 1) {
+            return false;
+        }
+        return TryBuildStructure(ABILITY_ID::BUILD_ENGINEERINGBAY);
+    }
+    bool TryBuildArmory() {
+        if (CountUnitType(UNIT_TYPEID::TERRAN_ARMORY) > 1) {
+            return false;
+        }
+        return TryBuildStructure(ABILITY_ID::BUILD_ARMORY);
+    }
+    bool TryBuildCC() {
+        return TryBuildStructure(ABILITY_ID::BUILD_COMMANDCENTER);
+    }
+
 };
 
 int main(int argc, char* argv[]) {
